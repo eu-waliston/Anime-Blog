6981d96d5f223cc851165536a28d9700c80c569e
Waliston Euripedes <82295321+wal-wizard@users.noreply.github.com>
2023-05-19 15:34:18 -0300

    Add files via upload

---
+++ Blog-Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      < Navbar />
+      <Home />
+      
+    </div>
+  )
+}
+
+export default App;
