2829135c740ccbc2c7d831cc35ea66429e2d3f88
Waliston <goneisgone@hotmail.com>
2023-08-17 20:39:08 -0300

    React Blog, Post Controller

--- blog-backend/src/Controller/Post.Controller.js
+++ blog-backend/src/Controller/Post.Controller.js
@@ -1,1 +1,71 @@
-//TODO Post Controller
+const Post = require("../Model/Post.Model");
+
+async function getAllPosts(req, res) {
+  try {
+    let allPosts = await Post.find();
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+async function getAnPost(req, res) {
+  let id = req.params.id;
+  try {
+    let post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+async function createAPost(req, res) {
+  const newPost = new Post({
+    post_title: req.body.post_title,
+    post_content: req.body.post_content,
+    post_author: req.body.post_author,
+    post_date: req.body.post_date,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+async function updatePost(req, res) {
+  const id = req.params.id;
+  try {
+    await Post.findByIdAndUpdate(id, {
+      post_title: req.body.post_title,
+      post_content: req.body.post_content,
+      post_author: req.body.post_author,
+      post_date: req.body.post_date,
+    });
+
+    let updatedPost = await Post.findById(id);
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+async function deletePost(req, res) {
+  let id = req.params.id;
+  try {
+    Post.findByIdAndDelete(id);
+    res.status(200).json("User Successfully delected!");
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+module.exports = {
+  getAllPosts,
+  getAnPost,
+  createAPost,
+  updatePost,
+  deletePost,
+};
--- blog-backend/src/Model/Post.Model.js
+++ blog-backend/src/Model/Post.Model.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const PostSchema = mongoose.Schema({
   post_title: { String, required: true },
   post_content: { String, required: true },
-  post_date: { String, required: true },
+  post_author: { String, required: true },
   post_date: { Date, required: true },
 });
 
