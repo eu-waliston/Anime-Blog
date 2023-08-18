d84599e8329a826f0a53020b6381983fe1d0694e
Waliston <goneisgone@hotmail.com>
2023-08-17 21:21:39 -0300

    React Blog, User View(routes)

--- blog-backend/src/View/User.View.js
+++ blog-backend/src/View/User.View.js
@@ -1,1 +1,18 @@
-//TODO View
+const express = require("express");
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../Controller/User.Controller");
+
+const UserRouter = express.Router();
+
+UserRouter.get("/blog/user", getAllUsers);
+UserRouter.get("/blog/user/:id", getUser);
+UserRouter.get("/blog/user", createUser);
+UserRouter.get("/blog/user/:id", updateUser);
+UserRouter.get("/blog/user/:id", deleteUser);
+
+module.exports = UserRouter;
