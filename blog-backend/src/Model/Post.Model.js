8b74606c4552b85df0f32eb21cf9cab496cb6003
wal-wizard <goneisgone@hotmail.com>
2023-09-12 20:39:32 -0300

    Update

--- blog-backend/src/Model/Post.Model.js
+++ blog-backend/src/Model/Post.Model.js
@@ -1,10 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 const PostSchema = mongoose.Schema({
-  post_title: { String, required: true },
-  post_content: { String, required: true },
-  post_author: { String, required: true },
-  post_date: { Date, required: true },
+  post_title: { String },
+  post_content: { String },
+  post_author: { String },
+  post_date: { Date },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
--- blog-backend/src/Model/User.Model.js
+++ blog-backend/src/Model/User.Model.js
@@ -1,11 +1,11 @@
 const { default: mongoose } = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    user_first_name: {String, required: true},
-    user_last_name: {String, required: true},
-    user_nasc_date: {Date, required: true},
-    user_email: {String, required: true},
-    user_password: {String, required: true}
+    user_first_name: { String },
+    user_last_name: { String },
+    user_nasc_date: { Date },
+    user_email: { String },
+    user_password: { String }
 })
 
 
