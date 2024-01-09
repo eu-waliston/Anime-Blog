daf99839af1de800951426d3ac648d43b3bb85e6
We Santos <wesantosdev@gmail.com>
2024-01-09 19:59:21 -0300

    Update

--- blog-frontend/src/routes/routes.js
+++ blog-frontend/src/routes/routes.js
@@ -1,9 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 
-import Login from "../components/User/Login";
-import Recover from "../components/User/Recover";
-import Register from "../components/User/Register";
 import Navbar from "../components/Navbar/Navbar";
 import Home from "../components/Home/Home";
 
@@ -11,11 +8,6 @@ const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={[<Navbar />, <Home /> ]} />
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/recover" element={<Recover />} />
-      <Route path="/register" element={<Register />} />
-      
     </Routes>
   );
 };
