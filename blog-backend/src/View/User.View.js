6093463e007a877b74102b521ae128c31b189ad3
Waliston <wesantosdev@gmail.com>
2024-05-29 19:16:00 -0300

    update

--- blog-backend/src/View/User.View.js
+++ blog-backend/src/View/User.View.js
@@ -11,11 +11,11 @@ const {
     DeleteUser
 } = require("../Controller/User.Controller")
 
-UserRouter.get("/magic-library/api/v1/register", CreateUser)
-UserRouter.post("/magic-library/api/v1/login", RegisterUser)
-UserRouter.get("/magic-library/api/v1/get-a-user/:id", GetUser)
-UserRouter.get("/magic-library/api/v1/get-all-users", GetAllUsers)
-UserRouter.put("/magic-library/api/v1/update-a-user/:id", UpdateUser)
-UserRouter.delete("/magic-library/api/v1/delete-a-user/:id", DeleteUser)
+UserRouter.get("/anime-blog/api/v1/register", CreateUser)
+UserRouter.post("/anime-blog/api/v1/login", RegisterUser)
+UserRouter.get("/anime-blog/api/v1/get-a-user/:id", GetUser)
+UserRouter.get("/anime-blog/api/v1/get-all-users", GetAllUsers)
+UserRouter.put("/anime-blog/api/v1/update-a-user/:id", UpdateUser)
+UserRouter.delete("/anime-blog/api/v1/delete-a-user/:id", DeleteUser)
 
 module.exports = UserRouter;
